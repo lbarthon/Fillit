eb89d9d8507a86327d26109484409bbbf58d29bf
Pelt10 <pelt10@live.fr>
2018-11-21 00:06:30 +0100

    Some tries...

--- srcs/solver.c
+++ srcs/solver.c
@@ -6,7 +6,7 @@
 /*   By: ple-thie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 13:54:43 by ple-thie          #+#    #+#             */
-/*   Updated: 2018/11/20 16:15:05 by ple-thie         ###   ########.fr       */
+/*   Updated: 2018/11/21 00:06:02 by ple-thiec        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,22 @@ static short ft_extract_shape(void *map, size_t map_size, int loc)
 {
 	short			extract;
 	unsigned int	len;	
-	
-	extract = 0;
-	len = loc + 4 > map_size ? map_size - loc : 4;
-	while (1)
-	{
-		map = map << loc;
+	int				i;
+	int				n;
 
+	len = loc + 4 > map_size ? map_size - loc : 4;
+	n= 0;
+	while (n == 4)
+	{
+		*map = *map << loc;
+		while (i < len)
+		{
+			extract &= (*map & 1) << (4 * n + i);
+			i++;
+		}
+		n++;
 	}
+	return extract;
 }
 
 char	**ft_solve(short *shapes, int map_size, void *map, char **return_map)
